148729479d3db60d61933e920b31186c2a0b1d30
APCSodela <adrianpaulsodela0@gmail.com>
2020-03-12 23:22:18 +0800

    03-12-2020

--- src/main/java/com/pup/cea/facs/controller/TicketController.java
+++ src/main/java/com/pup/cea/facs/controller/TicketController.java
@@ -15,7 +15,6 @@ import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.pup.cea.facs.model.Ticket;
 import com.pup.cea.facs.service.TicketService;
